2ebb8c811eca71cdf69d47b5d159c5d3c5ddc3c7
Rimas Alghamdi <reemas.y24@gmail.com>
2023-09-06 21:40:30 +0300

    V2

--- Sorter.java
+++ Sorter.java
@@ -1,5 +1,37 @@
 public class Sorter {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] numbers = { 5, 2, 8, 3, 1, 9, 4, 7, 0 };
+
+        System.out.println("Original array:");
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        selection(numbers);
+
+        System.out.println("Sorted array:");
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public static void selection(int[] array) {
+        int n = array.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
     }
 }
